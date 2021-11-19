f8f975f25dac08aef01999471e5ae7cd1ad06307
Catherine Meade <cat@heysparkbox.com>
2021-11-19 17:21:58 -0500

    feat: update work

--- src/components/Main.js
+++ src/components/Main.js
@@ -33,27 +33,27 @@ class Main extends React.Component {
             <img src={pic01} alt="" />
           </span>
           <p>
-            I graduated from Miami University's University Honors Program with a double-major in English and Psychology for undergrad, 
-            and then graduated from the University of Dayton with my Masters of Business Administration. I am a storyteller at heart. 
-            I became an English major because of the way that it ties stories to life. If it has been experienced, chances are it has been written about. 
-            Stories are how we connect to other people. We tell each other stories about what happened, how it made us feel, why we acted the way we did. 
-            Project Management is listening to stories, adapting plans accordingly, and retelling those stories where needed. 
-            There is no part of life that is not, at its core, storytelling. 
+            I graduated from Miami University's University Honors Program with a double-major in English and Psychology for undergrad,
+            and then graduated from the University of Dayton with my Masters of Business Administration. I am a storyteller at heart.
+            I became an English major because of the way that it ties stories to life. If it has been experienced, chances are it has been written about.
+            Stories are how we connect to other people. We tell each other stories about what happened, how it made us feel, why we acted the way we did.
+            Project Management is listening to stories, adapting plans accordingly, and retelling those stories where needed.
+            There is no part of life that is not, at its core, storytelling.
           </p>
           <p>
-            Storytelling is an art. A story should strike the audience, grab them and not let go. It is a means of connecting with people, recognizing who they are, 
-            what they want, and how you can help each other. What is business, if not interacting with people? A storyteller is someone able to interact at any 
-            level--from the executive to the cleaning crew to the end-user. It's a matter of knowing and understanding the audience, of connecting with them and 
-            having a dialogue. With that skill, a project manager doesn't need the technical knowledge, because they understand the people they're working with and 
-            what the SMEs are saying. A good project manager can track and guide and lead a project from start to finish, navigating through the inevitable issues and 
-            risks with a level head, a solid team, and a mitigation plan--because they can communicate effectively. For the project manager, it isn't about them; 
-            it's about the project, the end result, the team, and the end-users. The stakeholders are the audience, and the project manager needs to tell them 
+            Storytelling is an art. A story should strike the audience, grab them and not let go. It is a means of connecting with people, recognizing who they are,
+            what they want, and how you can help each other. What is business, if not interacting with people? A storyteller is someone able to interact at any
+            level--from the executive to the cleaning crew to the end-user. It's a matter of knowing and understanding the audience, of connecting with them and
+            having a dialogue. With that skill, a project manager doesn't need the technical knowledge, because they understand the people they're working with and
+            what the SMEs are saying. A good project manager can track and guide and lead a project from start to finish, navigating through the inevitable issues and
+            risks with a level head, a solid team, and a mitigation plan--because they can communicate effectively. For the project manager, it isn't about them;
+            it's about the project, the end result, the team, and the end-users. The stakeholders are the audience, and the project manager needs to tell them
             the project's story.
           </p>
           <p>
-            A story is not static, and a storyteller cannot be either. I taught myself to use Microsoft Project well enough that I was sought out to train others in 
-            the Institute on how to use it. Not all projects are the traditional waterfall style, and so I became a Professional Scrum Master I as well. 
-            There are always new methods of finding and presenting information to find and adapt. Technology is evolving, and so too are our stories. 
+            A story is not static, and a storyteller cannot be either. I taught myself to use Microsoft Project well enough that I was sought out to train others in
+            the Institute on how to use it. Not all projects are the traditional waterfall style, and so I became a Professional Scrum Master I as well.
+            There are always new methods of finding and presenting information to find and adapt. Technology is evolving, and so too are our stories.
             Let me help you bring your story to life.
           </p>
           <p>
@@ -75,19 +75,7 @@ class Main extends React.Component {
           </span>
           <p className="image-caption">Dayton, Ohio via <a href="https://unsplash.com/@bowmanmc">M. Bowman</a></p>
           <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
-          </p>
-          <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
+            I'm currently a project manager with approximately 10 years of PM experience, 7 of which have been with the Department of Defense (DoD). I am formally trained in both waterfall and Agile styles, focusing on achieving customer goals with multifunctional teams. <a href="mailto:Andrew.croftcheck@gmail.com?Subject=From%20your%20website">Send me a message</a> if you'd like to know more.
           </p>
           {close}
         </article>
@@ -104,9 +92,9 @@ class Main extends React.Component {
             <img src={pic03} alt="" />
           </span>
           <p>
-            I'm Andrew Croftcheck, a Project Manager. I live in Dayton, OH with my partner and our two cats. 
-            In my spare time I enjoy playing video games and board games, reading, playing tabletop RPGs, and helping out at 4 Paws for Ability. 
-            I did my undergrad at Miami, double-majoring in English and Psychology, and then picked up an MBA from the University of Dayton later on. 
+            I'm Andrew Croftcheck, a Project Manager. I live in Dayton, OH with my partner and our two cats.
+            In my spare time I enjoy playing video games and board games, reading, playing tabletop RPGs, and helping out at 4 Paws for Ability.
+            I did my undergrad at Miami, double-majoring in English and Psychology, and then picked up an MBA from the University of Dayton later on.
           </p>
           {close}
         </article>
